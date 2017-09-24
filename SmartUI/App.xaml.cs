792cc24dde4eee64423e5981fa3cfbbe32fb89ba
ulyanov yuriy <ulyanov.yuriy@live.com>
2017-09-24 22:02:01 +0300

    default theme

--- SmartUI/App.xaml.cs
+++ SmartUI/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,13 @@ namespace SmartUI
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ThemeManager.ChangeAppStyle(Application.Current,
+                                    ThemeManager.GetAccent("Blue"),
+                                    ThemeManager.GetAppTheme("BaseDark"));
+
+            base.OnStartup(e);
+        }
     }
 }
